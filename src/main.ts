3e8e9d3b2b45db009cb826b0a0a8ceaa8255a27d
Dat Thai <dat_thai@datahouse.com>
2024-04-03 00:23:25 +0700

    init project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,60 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import * as compression from 'compression';
+import { ApiConfig, AppConfig } from './config';
+import { useContainer } from 'class-validator';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { prefix, version } = app.get(ApiConfig);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  //init middleware
+  app.use(helmet());
+  app.use(compression());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  //set prefix for all url and api
+  app.setGlobalPrefix(prefix);
+
+  //setup swagger
+  const config = new DocumentBuilder()
+    .setTitle('Percies shopee example')
+    .setDescription('The percies shopee API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${prefix}/swagger`, app, document);
+
+  const appConfig = app.get(AppConfig);
+  const server = await app.listen(appConfig.port);
+
+  server.keepAliveTimeout = 65 * 1000; // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
+  server.headersTimeout = 66 * 1000; // Ensure the headersTimeout is set higher than the keepAliveTimeout due to this nodejs regression bug: https://github.com/nodejs/node/issues/27363
+
+  // Log Swagger URL
+  const url = await app.getUrl();
+  Logger.log(`Swagger UI is available at ${url}/${prefix}/swagger`);
+
+  return appConfig;
 }
-bootstrap();
+
+bootstrap()
+  .then((appConfig: AppConfig) => {
+    new Logger('Bootstrap').log(
+      { appConfig },
+      `Server is listening on port ${appConfig.port}, environment=${appConfig.env}`,
+    );
+  })
+  .catch((err) => {
+    new Logger('Bootstrap').error(`Error starting server, ${err}`);
+    throw err;
+  });
